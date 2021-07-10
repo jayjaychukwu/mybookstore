b71eb01006ca3a3c51a51127263d1fdcd6685800
jayjaychukwu <liljaydon.pj@gmail.com>
2021-07-10 16:03:25 +0100

    New Edit again

--- mybookstore/settings.py
+++ mybookstore/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'mybookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
