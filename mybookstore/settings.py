6d2482d845ccf4f04bfd0e4538edef9b2c04b6c0
jayjaychukwu <liljaydon.pj@gmail.com>
2021-07-10 16:49:38 +0100

    Newdom

--- mybookstore/settings.py
+++ mybookstore/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=!1zt*$$x$9&)d7h13f^8+oya5ikumgo)m3z(2m=)@%ia8)^!g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
